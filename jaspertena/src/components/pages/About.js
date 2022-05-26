a475eef6e92c2747b632dcd88ed3b896a6b6450b
Maddox Twin1 <jaspertena@aol.com>
2022-05-26 10:03:58 -0400

    file structure set up

---
+++ jaspertena/src/components/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+      As a recent Georgia Tech Bootcamp full stack web developer graduate, I am 
+      looking to  leverage an accounting and finance background to build a more 
+      intuitive user experience on the web. I aim to use my newly developed 
+      skills in JavaScript, CSS, React.js, SQL, Node.js,  MongoDB and responsive
+      web design along with seasoned interpersonal, problem solving and critical 
+      thinking skills to add value in a team environment.  I have worked on  
+      teams to build full stack websites from creations  of concepts to 
+      fully deployed sites. 
+      </p>
+    </div>
+  );
+}
